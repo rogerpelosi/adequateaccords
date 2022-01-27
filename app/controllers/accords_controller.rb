465483a6cbfb04c1f346f27cb51c8f2524961395
roger chat <mattrogerpelosi@gmail.com>
2022-01-27 16:04:42 -0500

    adding resource routes

--- app/controllers/accords_controller.rb
+++ app/controllers/accords_controller.rb
@@ -1,15 +1,22 @@
 class AccordsController < ApplicationController
 
     def create 
-
+        @new_accord = Accord.new(accord_params)
+        if @new_accord.save
+            render json: @new_accord, 
+            status: :created
+        else 
+            render json: @new_accord.errors.full_messages, 
+            status: :unprocessable_entity
+        end 
     end 
 
     def index 
-
+        render json: Accord.all.order(created_at: :desc)
     end 
 
     def show 
-
+        render json: @accord
     end 
 
     private 
