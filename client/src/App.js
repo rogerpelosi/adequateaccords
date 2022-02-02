9e5876c4ff391284661f98b20f9f27b4fcc2f939
roger chat <mattrogerpelosi@gmail.com>
2022-02-02 11:54:19 -0500

    adding components to client / view, editing App.js and imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import AboutPage from './components/AboutPage';
+import EmailList from './components/EmailList';
+import NewEmail from './components/NewEmail';
+
 function App() {
+
+  const [emails, setEmails] = useState([]);
+
+  useEffect(() => {
+
+  },[])
+
+//kmsdfkmskdmfkmdfh
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <AboutPage />
+      <EmailList />
+      <NewEmail />
+
     </div>
   );
 }
